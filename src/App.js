d559abea6ba47469c10b0c3d8f7807bf09f2b71c
Sealman <castle2668@gmail.com>
2021-12-09 15:44:53 +0800

    Fetch movie data from SWAPI

--- src/App.js
+++ src/App.js
@@ -1,31 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+
+  function fetchMovieHandler() {
+    // 使用 fetch 透過網路取得 json
+    fetch('https://swapi.dev/api/films')
+      .then((res) => {
+        // 回傳的 response 需要先透過 json() 轉換
+        return res.json();
+      })
+      // 然後就能開始使用資料
+      .then((data) => {
+        // 只取用我們需要的資料，不要把整包 API 資料帶走，減少複雜度
+        const transformedMovies = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+        setMovies(transformedMovies);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
