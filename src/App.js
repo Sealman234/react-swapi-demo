f9c03eae10a86ad0ff9b78a2ccf5f9b416499b3a
Sealman <castle2668@gmail.com>
2021-12-09 16:02:10 +0800

    Use Fetch with async/await

--- src/App.js
+++ src/App.js
@@ -6,29 +6,19 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
 
-  function fetchMovieHandler() {
-    // 使用 fetch 透過網路取得 json
-    fetch('https://swapi.dev/api/films')
-      .then((res) => {
-        // 回傳的 response 需要先透過 json() 轉換
-        return res.json();
-      })
-      // 然後就能開始使用資料
-      .then((data) => {
-        // 只取用我們需要的資料，不要把整包 API 資料帶走，減少複雜度
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        setMovies(transformedMovies);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  async function fetchMovieHandler() {
+    const response = await fetch('https://swapi.dev/api/films');
+    const data = await response.json();
+
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovies);
   }
 
   return (
